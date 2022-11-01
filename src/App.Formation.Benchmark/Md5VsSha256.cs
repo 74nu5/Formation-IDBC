4b8678a940aad65ea5d4a89a938a21d74cc67781
Romain Avonde <romain@avonde.email>
2022-11-01 20:34:01 +0100

    Add benchmark

---
+++ src/App.Formation.Benchmark/Md5VsSha256.cs
@@ -0,0 +1,37 @@
+﻿namespace App.Formation.Benchmark;
+
+using System;
+using System.Security.Cryptography;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+[SimpleJob(RuntimeMoniker.Net472, baseline: true)]
+[SimpleJob(RuntimeMoniker.Net48)]
+[SimpleJob(RuntimeMoniker.NativeAot70)]
+[SimpleJob(RuntimeMoniker.Net60)]
+[SimpleJob(RuntimeMoniker.Net70)]
+public class Md5VsSha256
+{
+    private readonly SHA256 sha256 = SHA256.Create();
+
+    private readonly MD5 md5 = MD5.Create();
+
+    private byte[] data;
+
+    [Params(1000, 10000)]
+    public int N;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        this.data = new byte[this.N];
+        new Random(42).NextBytes(this.data);
+    }
+
+    [Benchmark]
+    public byte[] Sha256() => this.sha256.ComputeHash(this.data);
+
+    [Benchmark]
+    public byte[] Md5() => this.md5.ComputeHash(this.data);
+}
---
+++ src/App.Formation.Benchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using App.Formation.Benchmark;
+
+using BenchmarkDotNet.Running;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<Md5VsSha256>();
+    }
+}
